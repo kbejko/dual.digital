1e25768f7103ff1082bd3cc027faa91ee56c7046
kbejko <kbejko@gmail.com>
2017-06-04 12:35:42 -0400

    Playing with anime js

--- javascript/script.js
+++ javascript/script.js
@@ -1,78 +1,30 @@
-Barba.Pjax.start()
+// import anime from 'animejs'
 
-var FadeTransition = Barba.BaseTransition.extend({
-  start: function() {
-    /**
-     * This function is automatically called as soon the Transition starts
-     * this.newContainerLoading is a Promise for the loading of the new container
-     * (Barba.js also comes with an handy Promise polyfill!)
-     */
+anime({
+  targets: '#dual-left',
+  translateX: '-1vw',
+  easing: 'easeInOutQuad',
+  direction: 'alternate',
+  loop: 4,
+  rotate: 360
+})
 
-    // As soon the loading is finished and the old page is faded out, let's fade the new page
-    Promise
-      .all([this.newContainerLoading, this.fadeOut()])
-      .then(this.fadeIn.bind(this));
-  },
+anime({
+  targets: '#dual-right',
+  translateX: 20,
+  easing: 'easeInOutQuad',
+  direction: 'alternate',
+  loop: 4,
+  scale: 0,
+  rotate: 360
+})
 
-  fadeOut: function() {
-    /**
-     * this.oldContainer is the HTMLElement of the old Container
-     */
-
-    return $(this.oldContainer).animate({ opacity: 0 }).promise();
-  },
-
-  fadeIn: function() {
-    /**
-     * this.newContainer is the HTMLElement of the new Container
-     * At this stage newContainer is on the DOM (inside our #barba-container and with visibility: hidden)
-     * Please note, newContainer is available just after newContainerLoading is resolved!
-     */
-
-    var _this = this;
-    var $el = $(this.newContainer);
-
-    $(this.oldContainer).hide();
-
-    $el.css({
-      visibility : 'visible',
-      opacity : 0
-    });
-
-    $el.animate({ opacity: 1 }, 400, function() {
-      /**
-       * Do not forget to call .done() as soon your transition is finished!
-       * .done() will automatically remove from the DOM the old Container
-       */
-
-      _this.done();
-    });
-  }
-});
-
-/**
- * Next step, you have to tell Barba to use the new Transition
- */
-
-Barba.Pjax.getTransition = function() {
-  /**
-   * Here you can use your own logic!
-   * For example you can use different Transition based on the current page or link...
-   */
-
-  return FadeTransition;
-};
-
-// Checks current path name(url) and matches it current a's href
-// (function underlineCurrentTab(){
-//   var navLinks = document.querySelectorAll('.site__nav--links li a')
-//   var pathName = location.pathname.split('/')[1]
-//
-//   for (var i = 0; i < navLinks.length; i++) {
-//     if (pathName === navLinks[i].pathname.split('/')[1]) {
-//       navLinks[i].classList.add('active')
-//     }
-//   }
-// })();
-
-console.log(':)')
+anime({
+  targets: 'svg path',
+  fill: [
+    {value: '#f88199'},
+    {value: '#fdb134'},
+    {value: '#afccdf'}
+  ],
+  direction: 'alternate'
+})
